9d25f1a8c33b2b59bb22f862bd177ef7f32a4870
NickLindstromB3 <nick.lindstrom@b3.se>
2026-09-15 12:05:15 +0200

    updated path on showcase theme

--- src/constants/siteThemes.js
+++ src/constants/siteThemes.js
@@ -1,28 +1,28 @@
 const SITE_THEMES = Object.freeze({
   classic: {
-    id: 'classic',
-    label: 'Klassisk',
-    templatePath: 'themes/classic/template.html',
-    stylesheetPath: 'themes/classic/main.css'
+    id: "classic",
+    label: "Klassisk",
+    templatePath: "themes/classic/template.html",
+    stylesheetPath: "themes/classic/main.css",
   },
   editorial: {
-    id: 'editorial',
-    label: 'Editorial',
-    templatePath: 'themes/editorial/template.html',
-    stylesheetPath: 'themes/editorial/main.css'
+    id: "editorial",
+    label: "Editorial",
+    templatePath: "themes/editorial/template.html",
+    stylesheetPath: "themes/editorial/main.css",
   },
   showcase: {
-    id: 'showcase',
-    label: 'Showcase',
-    templatePath: 'themes/showcase/template.html',
-    stylesheetPath: 'themes/showcase/themes/showcase/main.css'
-  }
+    id: "showcase",
+    label: "Showcase",
+    templatePath: "themes/showcase/template.html",
+    stylesheetPath: "themes/showcase/main.css",
+  },
 });
 
 const SITE_THEME_IDS = Object.freeze(Object.keys(SITE_THEMES));
-const DEFAULT_SITE_THEME = 'classic';
-const SITE_THEME_MODE_IDS = Object.freeze(['light', 'dark']);
-const DEFAULT_SITE_THEME_MODE = 'light';
+const DEFAULT_SITE_THEME = "classic";
+const SITE_THEME_MODE_IDS = Object.freeze(["light", "dark"]);
+const DEFAULT_SITE_THEME_MODE = "light";
 
 function normalizeSiteTheme(value) {
   return SITE_THEMES[value] ? value : DEFAULT_SITE_THEME;
@@ -44,5 +44,5 @@ module.exports = {
   DEFAULT_SITE_THEME_MODE,
   normalizeSiteTheme,
   normalizeSiteThemeMode,
-  getSiteTheme
+  getSiteTheme,
 };
